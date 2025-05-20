bef84ee329f01e74274cc559b5e3a8ab78767471
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2025-05-20 15:54:40 -0400

    added intent to start MainActivity

--- app/src/main/java/ca/georgiancollge/ice3/SplashActivity.kt
+++ app/src/main/java/ca/georgiancollge/ice3/SplashActivity.kt
@@ -1,6 +1,10 @@
 package ca.georgiancollge.ice3
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -22,5 +26,14 @@ class SplashActivity : AppCompatActivity()
          v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
          insets
       }
+
+      Handler(Looper.getMainLooper()).postDelayed({
+         startActivity(Intent(this, MainActivity::class.java))
+         finish()
+      }, 3000)
+
+      val spinAnimation = AnimationUtils.loadAnimation(this, R.anim.spin)
+      binding.spinnerIcon.startAnimation(spinAnimation)
+
    }
 }
